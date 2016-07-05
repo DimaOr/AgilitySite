1278a16fe4a88f135309f3bc5f533c88acdfa19d
Дмитрий <marker.dim@gmail.com>
2016-07-05 12:07:42 +0300

    Refact carusel

--- js/main.js
+++ js/main.js
@@ -6,33 +6,29 @@ function Carusel(option) {
   this._element = option.element;
   this._leftButton = this._element.querySelector(".carusel_left");
   this._rightButton = this._element.querySelector(".carusel_right");
-  this._content =this._element.querySelector(".carusel_content");
-
-  this._rightButton.onclick=this.turnRight.bind(this);
-  this._leftButton.onclick=this.turnLeft.bind(this);
-
-
+  this._content = this._element.querySelector(".carusel_content");
+  this._backgroundMaxWidth = parseInt(window.getComputedStyle(this._content).backgroundSize.split(',')[0]);
+  this.minPositionX = -this._backgroundMaxWidth + this._content.clientWidth;
+  this.maxPositionX = 0;
+  this.stepTurn = this._content.clientWidth;
+  this._currentPositionX = +this._content.style.backgroundPositionX;
+  this._rightButton.onclick = this.turnRight.bind(this);
+  this._leftButton.onclick = this.turnLeft.bind(this);
 }
-Carusel.prototype.turnRight=function(event){
-  console.log(this._content.style.backgroundPositionX.toString());
-  var position =parseInt(this._content.style.backgroundPositionX)|| 0;
-  console.log(position);
-  if (position>-1474){
-    position = (position-737)+ "px";
-    this._content.style.backgroundPositionX=position;
+Carusel.prototype.turnRight = function (event) {
+  if (this._currentPositionX > this.minPositionX) {
+    this._currentPositionX -= this.stepTurn;
+    this._content.style.backgroundPositionX = (this._currentPositionX) + 'px';
   }
 }
 
-Carusel.prototype.turnLeft=function(event){
-  console.log(this._content.style.backgroundPositionX.toString());
-  var position =parseInt(this._content.style.backgroundPositionX)|| 0;
-  console.log(position);
-  if (position<0){
-    position = (position+737)+ "px";
-    this._content.style.backgroundPositionX=position;
+Carusel.prototype.turnLeft = function (event) {
+  if (this._currentPositionX < 0) {
+    this._currentPositionX += this.stepTurn;
+    this._content.style.backgroundPositionX = (this._currentPositionX) + 'px';
   }
 }
 
-var carusel =new Carusel({
+var carusel = new Carusel({
   element: document.querySelector(".carusel")
 });
